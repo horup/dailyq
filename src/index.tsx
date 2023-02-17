eb5c8fc291692f5eb88730f354f2a13eb41150ed
Søren Alsbjerg Hørup <sah@sah.dk>
2023-02-17 08:48:38 +0100

    added page support

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,9 @@ import Grid from '@mui/material/Grid';
 import {Pages, pageState} from './state';
 import { RecoilRoot, useRecoilState } from 'recoil';
 import { Configure } from './configure';
+import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AnalyticsIcon from '@mui/icons-material/Analytics';
 
 const darkTheme = createTheme({
   palette: {
@@ -40,10 +43,12 @@ function App() {
           {page == Pages.Configure && <Configure/>} 
         </Container>
       </div>
-      <BottomNavigation showLabels className='bottom'>
-        <BottomNavigationAction label="Questions" onClick={()=>setPage(Pages.Questions)} />
-        <BottomNavigationAction label="Analytics" onClick={()=>setPage(Pages.Analytics)} />
-        <BottomNavigationAction label="Configure" onClick={()=>setPage(Pages.Configure)} />
+      <BottomNavigation showLabels className='bottom' value={page} onChange={(event, newValue) => {
+        setPage(newValue as Pages);
+      }}>
+        <BottomNavigationAction label="Questions" icon={<QuestionMarkIcon />} />
+        <BottomNavigationAction label="Analytics" icon={<AnalyticsIcon />} />
+        <BottomNavigationAction label="Configure" icon={<SettingsIcon />} />
       </BottomNavigation>
     </div>
   </ThemeProvider>;
