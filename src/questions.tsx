192c553730e9f6685199112683234c864dc0b3a0
Søren Alsbjerg Hørup <sah@sah.dk>
2023-02-20 13:30:39 +0100

    added working scoring for today

--- src/questions.tsx
+++ src/questions.tsx
@@ -4,13 +4,21 @@ import { dateKeyToString } from './state';
 import * as state from './state';
 import {useRecoilState} from 'recoil';
 import Typography from '@mui/material/Typography';
-import { Button, Divider, Paper, Table, TableBody, TableCell, TableContainer, TableRow } from '@mui/material';
+import { Button, Dialog, Divider, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableRow } from '@mui/material';
 
 export function Questions() {
+    const [selectedQuestion, setSelectedQuestion] = React.useState<number | null>(null);
     const [dateKey, setDateKey] = useRecoilState(state.dateKeyState);
     const [questions, setQuestions] = useRecoilState(state.questionsState);
-    const today = DateTime.now().toLocal();
 
+    const onClickScore = (scoreValue:number) =>{
+        if (selectedQuestion != null) {
+            let qs = JSON.parse(JSON.stringify(questions));
+            qs[selectedQuestion].score[dateKey] = scoreValue;
+            setQuestions(qs);
+            setSelectedQuestion(null);
+        }
+    }
 
     return <>
         <div style={{width:'100%', textAlign:'center'}}>
@@ -26,13 +34,22 @@ export function Questions() {
                         return (
                             <TableRow key={index}>
                                 <TableCell>{q.question}</TableCell>
-                                <TableCell style={{width:'auto', textAlign:'right'}}><Button size='small' variant='contained'>{score}</Button></TableCell>
+                                <TableCell style={{width:'auto', textAlign:'right'}}><Button onClick={()=>setSelectedQuestion(index)} size='small' variant='contained'>{score}</Button></TableCell>
                             </TableRow>
                         )
                     })}
                 </TableBody>
             </Table>
         </TableContainer>
-
+        <Dialog open={selectedQuestion != null} onClose={()=>setSelectedQuestion(null)}>
+            <Paper>
+                <Grid padding={2} container={true} spacing={2}>
+                    {[10, 9, 8, 7, 6, 5, 4, 3, 2, 1].map((scoreValue, index)=>{
+                        return  <Grid key={index} item xs={12}><Button onClick={()=>onClickScore(scoreValue)} variant='contained' fullWidth>{scoreValue}</Button></Grid>
+                    })}
+                    
+                </Grid>
+            </Paper>
+        </Dialog>
     </>
 }
