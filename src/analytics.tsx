02f29304a10cd0cff256f375582fbe3482c264ea
Søren Alsbjerg Hørup <sah@sah.dk>
2023-02-22 09:29:05 +0100

    improved look and feel

--- src/analytics.tsx
+++ src/analytics.tsx
@@ -2,21 +2,13 @@ import { Box, Grid, Typography } from '@mui/material';
 import { DateTime } from 'luxon';
 import * as React from 'react';
 import { useRecoilState } from 'recoil';
+import { getColor } from './colors';
 import * as state from './state';
 
 export function Score({ scoreValue }: { scoreValue: number | undefined }) {
-    let reds = [] as number[];
-    let greens = [] as number[];
-    for (let i = 0; i <= 10; i++) {
-        greens[i] = 255 / 10 * i;
-        reds[i] = 255 - 255 / 10 * i;
-    }
-    let color = 'lightgrey';
-    if (scoreValue != null) {
-        color = `rgb(${reds[scoreValue]}, ${greens[scoreValue]}, 0)`;
-    }
+    let color = getColor(scoreValue as any);
     return (
-        <Typography style={{ color: color }}>{scoreValue != null ? scoreValue : '--'}</Typography>
+        <Typography variant='caption' style={{ color: color }}>{scoreValue != null ? scoreValue : '--'}</Typography>
     )
 }
 
@@ -34,11 +26,11 @@ export function Analytics() {
             return (
                 <React.Fragment key={index}>
                     <Grid xs={12} item key={index}>
-                        <Typography variant='body1' fontWeight={600}>{q.question}</Typography>
+                        <Typography variant='body1'>{q.question}</Typography>
                     </Grid>
                     {days.map((day) => {
                         return (
-                            <Grid textAlign={'left'} style={{ marginBottom: '32px' }} key={day} item xs={1}>
+                            <Grid textAlign={'left'} key={day} item xs={1}>
                                 <Score scoreValue={q.score[day]} />
                             </Grid>
                         )
---
+++ src/colors.tsx
@@ -0,0 +1,20 @@
+const colors = [
+    '#fb4764',
+    '#ff684e',
+    '#ff9232',
+    '#ffd708',
+    '#ffec01',
+    '#e8ec0a',
+    '#c1e420',
+    '#90d240',
+    '#64bf5d',
+    '#30a980'
+]
+
+export function getColor(scoreValue:undefined | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10) {
+    if (scoreValue === undefined) {
+        return 'lightgrey';
+    } else {
+        return colors[scoreValue - 1];
+    }
+}
